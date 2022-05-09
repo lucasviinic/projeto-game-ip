f70a3069975042ed9cbdceb3c7931fda4c16ef5d
lucasviinic <lvms@cin.ufpe.br>
2022-05-09 19:13:07 -0300

    refactor: correção na criação do banco e métodos do cCRUD agora em um contexto

--- services/manager.py
+++ services/manager.py
@@ -1,83 +1,92 @@
+import os.path
 import sqlite3
 
 
-connection = sqlite3.connect('database.db')
-cur = connection.cursor()
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+db_path = os.path.join(BASE_DIR, "database.db")
 
-def create_table():
-    '''
-        Cria a tabela players
-    '''
+with sqlite3.connect(db_path) as connection:
 
-    cur.execute('''
-        CREATE TABLE IF NOT EXISTS players (
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            nick TEXT NOT NULL,
-            score INTEGER NOT NULL
-        );
-    ''')
+    cur = connection.cursor()
 
-def check_if_player_exists(nick: str):
-    ''' 
-        Retorna True se o nick já existe no banco de dados
+    def create_table():
+        '''
+            Cria a tabela players
+        '''
 
-        Parâmetros:
-            nick: nick do usuário
-    '''
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS players (
+                id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                nick TEXT NOT NULL,
+                score INTEGER NOT NULL
+            );
+        ''')
 
-    cur.execute("SELECT * FROM players WHERE nick = ?", (nick,))
+    def check_if_player_exists(nick: str):
+        ''' 
+            Retorna True se o nick já existe no banco de dados
 
-    player = cur.fetchone()
+            Parâmetros:
+                nick: nick do usuário
+        '''
 
-    if player:
+        cur.execute("SELECT * FROM players WHERE nick = ?", (nick,))
+
+        player = cur.fetchone()
+
+        if player:
+            return True
+        else:
+            return False
+
+    def insert_player(nick: str, score: int):
+        '''
+            Insere um novo usuário na tabela
+
+            Parâmetros:
+                nick: nick do usuário
+                score: score do usuário
+        '''
+
+        if check_if_player_exists(nick):
+            return False
+        
+        cur.execute("INSERT INTO players (nick, score) VALUES (?, ?)", (nick, score))
+        connection.commit()
+
         return True
-    else:
-        return False
 
-def insert_player(nick: str, score: int):
-    '''
-        Insere um novo usuário na tabela
+    def update_score(nick: str, score: int):
+        '''
+            Atualiza o score de um usuário
 
-        Parâmetros:
-            nick: nick do usuário
-            score: score do usuário
-    '''
+            Parâmetros:
+                nick: nick do usuário
+                score: score do usuário
+        '''
 
-    if check_if_player_exists(nick):
-        return False
-    
-    cur.execute("INSERT INTO players (nick, score) VALUES (?, ?)", (nick, score))
-    connection.commit()
+        cur.execute('''
+            UPDATE players SET score = ? WHERE nick = ?
+        ''', (score, nick))
 
-    return True
+        connection.commit()
+        return True
 
-def update_score(nick: str, score: int):
-    '''
-        Atualiza o score de um usuário
+    def get_players_sorted_by_score():
+        '''
+            Retorna todos os usuários ordenados por score
 
-        Parâmetros:
-            nick: nick do usuário
-            score: score do usuário
-    '''
+            Retorno:
+                Lista de tuplas com os dados dos usuários
+        '''
 
-    cur.execute('''
-        UPDATE players SET score = ? WHERE nick = ?
-    ''', (score, nick))
+        cur.execute('''
+            SELECT * FROM players ORDER BY score DESC
+        ''')
 
-    connection.commit()
-    return True
+        players = cur.fetchall()
+        return players
 
-def get_players_sorted_by_score():
-    '''
-        Retorna todos os usuários ordenados por score
-
-        Retorno:
-            Lista de tuplas com os dados dos usuários2
-    '''
-
-    cur.execute('''
-        SELECT * FROM players ORDER BY score DESC
-    ''')
-
-    players = cur.fetchall()
-    return players
+if __name__ == "__main__":
+    create_table()
+    print('Table "players" created successfully')
